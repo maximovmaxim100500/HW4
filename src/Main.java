7c7a7a4068d4dc71684d0a067aefec26d36bb42c
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-10-09 23:02:49 +0300

    выполняю домашку

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-    }
+        System.out.println("Задача 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он " +
+                    "совершеннолетний.");
+        }
+        else {
+            System.out.println("Если возраст человека равен " + age + ", то он " +
+                    "не достиг совершеннолетия, нужно немного подождать.");
+        }
+        System.out.println(" ");
+        System.out.println("Задача 2");
+int temperature = 4;
+if (temperature < 5) {
+    System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
+}
+else {
+    System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
+}
+        System.out.println(" ");
+        System.out.println("Задача 3");
+        int speed = 60;
+        if (speed > 60){
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф.");
+        }
+        else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно.");
+        }
+        System.out.println(" ");
+        System.out.println("Задача 4");
+}
 }
